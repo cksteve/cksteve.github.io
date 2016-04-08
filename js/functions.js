6aeeb4289d21808554246f3048ef26688d4845f3
Kiva <kiva515@foxmail.com>
2016-04-08 21:50:07 +0800

    Update functions.js

--- js/functions.js
+++ js/functions.js
@@ -1,7 +1,3 @@
-/*
- * http://love.hackerzhou.me
- */
-
 // variables
 var $win = $(window);
 var clientWidth = $win.width();
@@ -39,7 +35,7 @@ $(window).resize(function() {
 
 function timeElapse(date){
 	var current = Date();
-	var seconds = (Date.parse(current) - Date.parse(date)) / 1000;
+	var seconds = Math.abs((Date.parse(current) - Date.parse(date))) / 1000;
 	var days = Math.floor(seconds / (3600 * 24));
 	seconds = seconds % (3600 * 24);
 	var hours = Math.floor(seconds / 3600);
